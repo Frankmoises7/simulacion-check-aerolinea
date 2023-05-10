646b3e57d9b53c85ebfbbcfdde078cdb3b9de19e
Frank Islas <frank.islas.zuniga@ciisa.cl>
2023-05-10 03:36:31 -0400

    add comments and asign seat to passengers

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -6,11 +6,13 @@ class PageController {
 
     const flightId = parseInt(req.params.id);
 
+    //Obtengo la data del vuelo por el ID de la URL
     try {
       const data = await prisma.flight.findUnique({
         where: { flight_id: flightId },
       });
 
+      //Si no hay data, error 400  
       if (!data) {
         return res.status(404).json({
           code: 400,
@@ -18,20 +20,29 @@ class PageController {
         });
       }
 
+      //Obtengo los datos del BoardingPass en relacion al Id del vuelo
       const boardingPass = await prisma.boarding_pass.findMany({
         where: { flight_id: flightId },
       });
 
+      //Obtengo los ID de los passengers
       const passengerBoardingPassId = boardingPass.map((pass) => {
         const passengersId = pass.passenger_id;
         return passengersId;
       });
 
+
+      // Obtengo los passengers de la BD en relacion al ID (sin orden)
       const UnorderPassengers = await prisma.passenger.findMany({
         where: { passenger_id: { in: passengerBoardingPassId } },
       });
 
+      //Obtengo los SeatId ordenados de forma Ascendente
+      const seatId = await prisma.seat.findMany({
+        orderBy: [{seat_id: "asc"}]
+      })
 
+      // Le asigno las propiedades faltantes a los Passengers para mostrar
       UnorderPassengers.forEach((passenger) => {
         const boardingPassenger = boardingPass.find(
           (pass) => pass.passenger_id === passenger.passenger_id
@@ -40,10 +51,10 @@ class PageController {
           passenger.passengerBoardingPassId = boardingPassenger.boarding_pass_id;
           passenger.purchaseId = boardingPassenger.purchase_id;
           passenger.seatTypeId = boardingPassenger.seat_type_id;
-          passenger.seatId = null;
         }
       });
 
+      // Funcion para agrupar passengers por el seatType y ordenarlos por purchase
       function orderPassengers(passengers) {
         return passengers.sort((a, b) => {
           if (a.seatTypeId === b.seatTypeId) {
@@ -54,10 +65,16 @@ class PageController {
         });
       }
 
+      //Ordeno los passengers
       const passengers = orderPassengers(UnorderPassengers);
+
+      //Le asigno un seat a los passengers ordenados
+      passengers.forEach((passenger, i) => {
+          passenger.seatId = seatId[i].seat_id;
+      });
       
 
-
+      //Muestro toda la data
       res.status(200).json({
         code: 200,
         data,
